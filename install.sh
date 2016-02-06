1afe755c12e9ee129cec1e45970529acad6fd0b1
Roman <ArtRoman.Developer@gmail.com>
2016-02-06 23:58:53 +0300

    using yandex mirror

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ else
 fi
 echo -e $INFO Preparing a new Debian $iSystem chroot file base. Please, be patient,
 echo -e $INFO may takes a long time on low speed connection...
-debootstrap --no-check-gpg --no-check-certificate --variant=minbase --exclude=yaboot,udev,dbus,systemd --include=locales,mc,aptitude,wget,dialog,apt-utils,sysvinit-core,sysvinit,sysvinit-utils $iSystem $chrootBaseDir ftp://ftp.ru.debian.org/debian
+debootstrap --no-check-gpg --no-check-certificate --variant=minbase --exclude=yaboot,udev,dbus,systemd --include=locales,mc,aptitude,wget,dialog,apt-utils,sysvinit-core,sysvinit,sysvinit-utils $iSystem $chrootBaseDir http://mirror.yandex.ru/debian
 echo "share:x:1000:root,www-data,daapd" >> $chrootBaseDir/etc/group
 cat > $chrootBaseDir/usr/sbin/policy-rc.d <<EOF
 #!/bin/sh
